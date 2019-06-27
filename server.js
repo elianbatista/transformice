03cc17b66969383ba45b8e44b83d7b1a704dd15c
Elian Batista <elianbatista2000@gmail.com>
2019-06-27 18:10:56 -0300

    Criação do Server

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+http.listen(3000, function(){
+
+       console.log('Listening on port 3000');
+
+});
