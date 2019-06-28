2d3e58283bd8a2fd11bcd220dcc87ca4feae773b
LucasGobbs <llcostagobbi@gmail.com>
2019-06-27 22:48:10 -0300

    qualfoi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http);
-
+//ALOU ALOU
 http.listen(3000, function(){
 
        console.log('Listening on port 3000');
