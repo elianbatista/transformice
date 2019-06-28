179c7eec3a011c81a1bde4baae1761eb8f9ff9c8
Elian Batista <elianbatista2000@gmail.com>
2019-06-27 22:57:49 -0300

    Criação do chat local

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-
 const io = require('socket.io')(http);
-//ALOU ALOU
+
+app.get('/', (req,res)=>{
+
+       res.sendFile(__dirname+'/index.html');
+
+});
+
+io.on('connection', (socket)=>{
+
+       socket.on('msg', (msg)=>{
+
+              console.log(msg);
+
+              socket.broadcast.emit('msg', msg);
+
+       });
+
+});
+
 http.listen(3000, function(){
 
        console.log('Listening on port 3000');
